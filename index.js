bcc17d7334ad171a8e1b3220282e55868ebaa52c
Thai Xiong <thai303@hotmail.com>
2022-05-24 10:14:55 -0600

    commit tree 93569772ea8e7aa9048bb8fda3054d7256e6774e

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Add your code here
+function registerSelf() {
+    return fetch("http://guestbook.example.com/register", {
+      "method": "POST",
+      "body": {
+        "firstName": "THAI XIONG",
+        "registryMessage": "LET'S GO!"
+      }
+    }).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      return json.message;
+    });
+  }
+  
+function errorSelf() {
+    return fetch("http://guestbook.example.com/register-error", {
+      "method": "POST",
+      "body": {
+        "firstName": "  MENA XIONG",
+        "registryMessage": "BOSS LADY!"
+      }
+    }).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      return json.message;
+    }).catch(function(x) {
+      return ":(";
+    });
+}
+    
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+}
